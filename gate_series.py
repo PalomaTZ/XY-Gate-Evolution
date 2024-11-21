7fc9fba52ec5239b0c7a04a1152f260b159e7779
PalomaTZ <paloma.zuerlein@gmail.com>
2024-11-21 13:41:45 +0800

    testing

--- gate_series.py
+++ gate_series.py
@@ -5,10 +5,11 @@ import gate_class
 import random
 
 gate_type = ['X', 'Y', 'x', 'y']
-gate_list = ['X', 'X', 'X']#random.choices(gate_type, k=10)
+gate_list = random.choices(gate_type, k=10)
+print(gate_list)
 
 # I set t_0 to be 3*sigma (three standard deviations)
-args = {'W':4.5, 'W_d':4.5, 'A':0.0156776, 'b':0.4, 'sigma':80, 'alpha':-0.2, 'q':4}
+args = {'W':4.5, 'W_d':4.5, 'A':0.01568, 'b':0.4, 'sigma':80, 'alpha':-0.2, 'q':4}
 #can only change the step count of the time range
 time_range = np.linspace(0,6*args['sigma']*len(gate_list),200)
 qpsi0 = qt.basis(args['q'],0)
@@ -20,10 +21,14 @@ result = my_GateEvo.make_result()
 
 fig, ax = plt.subplots()
 
-ax.plot(time_range, result.expect[0])
-ax.plot(time_range, result.expect[1])
-ax.plot(time_range, result.expect[2])
-ax.plot(time_range, result.expect[3])
+ax.plot(time_range, result.expect[0], label = 'ground state')
+ax.plot(time_range, result.expect[1], label = '1st excited')
+ax.plot(time_range, result.expect[2], label = '2nd excited')
+ax.plot(time_range, result.expect[3], label = '3rd excited')
+ax.set_xlabel('Time')
+ax.set_ylabel('Population')
+
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=4)
 
 plt.show()
 
