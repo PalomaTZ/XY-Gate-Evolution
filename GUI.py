5bb3bb11d3e63dc6061505b0fbdae67bfe2bc8c5
HengYi-Lee <cpshs10911364@cpshs.hcc.edu.tw>
2024-11-28 13:31:32 +0800

    GUI.py

--- GUI.py
+++ GUI.py
@@ -14,15 +14,14 @@ GUItest = tk.Tk()
 GUItest.title("xyGate")
 GUItest.geometry("2500x1000")
 
-
-GUItest.rowconfigure(0, weight=1)
+GUItest.rowconfigure(0, weight=2)
 GUItest.columnconfigure(1, weight=1)
 GUItest.columnconfigure(3, weight=3)
 
 # Font settings for labels and entries
-font_settings = ("Arial", 16)
+font_settings = ("Arial", 24)
 # LabelFrame to group input fields
-input_frame = tk.LabelFrame(GUItest, text="Parameters", font=("Arial", 14, "bold"))
+input_frame = tk.LabelFrame(GUItest, text="Parameters", font=("Arial", 24, "bold"))
 input_frame.grid(row=0, column=0, rowspan=10, columnspan=2, padx=10, pady=10, sticky="nsew")
 
 
@@ -40,49 +39,49 @@ entry_width = 30
 # en = Entry
 
 
-lb_W = tk.Label(input_frame, text='Base Energy(W)', font=font_settings, width=entry_width)
+lb_W = tk.Label(input_frame, text='Transmon Frequency(W)', font=font_settings, width=entry_width)
 lb_W.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
 en_W = tk.Entry(input_frame, font=font_settings)
 en_W.grid(row=0, column=1, sticky="nsew", padx=5, pady=3)
 en_W.insert(0, str(4.5))
 
-lb_W_d = tk.Label(input_frame, text='Energy Offset(W_d)', font=font_settings, width=entry_width)
+lb_W_d = tk.Label(input_frame, text='Driving Frequency(W_d)', font=font_settings, width=entry_width)
 lb_W_d.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
 en_W_d = tk.Entry(input_frame, font=font_settings)
 en_W_d.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
 en_W_d.insert(0, str(4.5))
 
-lb_A = tk.Label(input_frame, text='Amplitude(A)', font=font_settings, width=entry_width)
+lb_A = tk.Label(input_frame, text='diving amplitude(A)', font=font_settings, width=entry_width)
 lb_A.grid(row=2, column=0, sticky=tk.W, padx=5, pady=5)
 en_A = tk.Entry(input_frame, font=font_settings)
 en_A.grid(row=2, column=1, sticky="nsew", padx=5, pady=5)
 en_A.insert(0, str(0.068))
 
-lb_b = tk.Label(input_frame, text='Derivative Intensity(b)', font=font_settings, width=entry_width)
+lb_b = tk.Label(input_frame, text='DRAG factor(b)', font=font_settings, width=entry_width)
 lb_b.grid(row=3, column=0, sticky=tk.W, padx=5, pady=5)
 en_b = tk.Entry(input_frame, font=font_settings)
 en_b.grid(row=3, column=1, sticky="nsew", padx=5, pady=5)
 en_b.insert(0, str(0.4))
 
-lb_sigma = tk.Label(input_frame, text='Gaussian Width(sigma)', font=font_settings, width=entry_width)
+lb_sigma = tk.Label(input_frame, text='gate length(sigma)', font=font_settings, width=entry_width)
 lb_sigma.grid(row=4, column=0, sticky=tk.W, padx=5, pady=5)
 en_sigma = tk.Entry(input_frame, font=font_settings)
 en_sigma.grid(row=4, column=1, sticky="nsew", padx=5, pady=5)
 en_sigma.insert(0, str(130))
 
-lb_t_0 = tk.Label(input_frame, text='Gaussian Center Time(t_0)', font=font_settings, width=entry_width)
+lb_t_0 = tk.Label(input_frame, text='gating time(t_0)', font=font_settings, width=entry_width)
 lb_t_0.grid(row=5, column=0, sticky=tk.W, padx=5, pady=5)
 en_t_0 = tk.Entry(input_frame, font=font_settings)
 en_t_0.grid(row=5, column=1, sticky="nsew", padx=5, pady=5)
 en_t_0.insert(0, str(500))
 
-lb_alpha = tk.Label(input_frame, text='Control Strength Coefficient(alpha)', font=font_settings, width=entry_width)
+lb_alpha = tk.Label(input_frame, text='anharmonicity(alpha)', font=font_settings, width=entry_width)
 lb_alpha.grid(row=6, column=0, sticky=tk.W, padx=5, pady=5)
 en_alpha = tk.Entry(input_frame, font=font_settings)
 en_alpha.grid(row=6, column=1, sticky="nsew", padx=5, pady=5)
 en_alpha.insert(0, str(-0.2))
 
-GUItest.option_add("*TCombobox*Listbox.font","Arial 16")
+GUItest.option_add("*TCombobox*Listbox.font","Arial 24")
 lb_gate = tk.Label(input_frame, text='Gate Type(gate)', font=font_settings, width=entry_width)
 lb_gate.grid(row=7, column=0, sticky=tk.W, padx=5, pady=5)
 values_gate = ("X" , "x" , "Y" , "y")
@@ -91,11 +90,16 @@ en_gate.grid(row=7, column=1, sticky="nsew", padx=5, pady=5)
 en_gate.set("x") 
 
 
-lb_q = tk.Label(input_frame, text='System Dimension(q)', font=font_settings, width=entry_width)
+lb_q = tk.Label(input_frame, text='number of levels(q)', font=font_settings, width=entry_width)
 lb_q.grid(row=8, column=0, sticky=tk.W, padx=5, pady=5)
 en_q = tk.Entry(input_frame, font=font_settings)
 en_q.grid(row=8, column=1, sticky="nsew", padx=5, pady=5)
 en_q.insert(0, str(4))
+
+check_btn_gausswave_var = tk.IntVar()
+check_btn_gausswave = tk.Checkbutton(input_frame,text='gauss wave',variable = check_btn_gausswave_var,font=font_settings, width=entry_width)
+check_btn_gausswave.grid(row=9, column=0, sticky=tk.W, padx=5, pady=5)
+check_btn_gausswave.pack
 
 # lb_t_r = tk.Label(input_frame, text='Time Range (start, end, num)', font=font_settings, width=entry_width)
 # lb_t_r.grid(row=9, column=0, sticky=tk.W, padx=5, pady=5)
@@ -122,13 +126,28 @@ en_q.insert(0, str(4))
 # Matplotlib figure and canvas for plot
 fig, ax = plt.subplots()
 canvas = FigureCanvasTkAgg(fig, master=GUItest)
-canvas.get_tk_widget().grid(row=0, column=3, rowspan=10, sticky="nsew", padx=10, pady=10)
+canvas.get_tk_widget().grid(row=0, column=3, rowspan=5, sticky="nsew", padx=10, pady=10)
 
 # solution Label
-solution_frame = tk.LabelFrame(GUItest, text="result", font=("Arial", 14, "bold"))
+solution_frame = tk.LabelFrame(GUItest, text="result", font=("Arial", 24, "bold"))
 solution_frame.grid(row=2, column=3, rowspan=10, columnspan=2, padx=10, pady=10, sticky="nsew")
-minimize_stste = tk.Label(solution_frame, text="minisize_state", font=("Arial", 14))
-minimize_stste.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
+
+for i in range(4):  
+    solution_frame.columnconfigure(i, weight=1)
+
+State_0 = tk.Label(solution_frame, text="P0 :", font=("Arial", 18))
+State_0.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+
+State_1 = tk.Label(solution_frame, text="P1 :", font=("Arial", 18))
+State_1.grid(row=0, column=2, sticky="nsew", padx=5, pady=5)
+
+State_2 = tk.Label(solution_frame, text="P2 :", font=("Arial", 18))
+State_2.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+
+State_3 = tk.Label(solution_frame, text="P3 :", font=("Arial", 18))
+State_3.grid(row=1, column=2, sticky="nsew", padx=5, pady=5)
+
+
 
 
 # Make the canvas resizable
@@ -169,21 +188,43 @@ def update_plot():
     
     my_GateEvo = gate_class.GateEvo(time_range,qpsi0,args)
     result = my_GateEvo.make_result()
-
     
-    
+    plt.rcParams.update({'font.size': 16})
+    global ax,ax2,fig
+    if fig is None or ax is None:
+        fig, ax = plt.subplots()  
+    else:
+        fig.clear()  
+        ax = fig.add_subplot(111) 
     ax.clear()
-    ax.plot(time_range, result.expect[0])
-    ax.plot(time_range, result.expect[1])
-    ax.plot(time_range, result.expect[2])
-    ax.plot(time_range, result.expect[3])
-
+    ax.plot(time_range, result.expect[0], color='#1f77b4', label="P0")
+    ax.plot(time_range, result.expect[1], color='#ff7f0e', label="P1")
+    ax.plot(time_range, result.expect[2], color='#2ca02c', label="P2")
+    ax.plot(time_range, result.expect[3], color='#d62728', label="P3")
     ax.set_xlabel("Time")
     ax.set_ylabel("Probability")
     ax.set_title("State Probabilities Over Time")
     ax.legend()
+    ax2 = ax.twinx()
+    check = check_btn_gausswave_var.get()
+    if check == 1:
+        # gauss_deriv = my_GateEvo.gauss_deriv(time_range)
+        gauss_wave = my_GateEvo.gauss_wave(time_range)
+        # ax2.plot(time_range,gauss_deriv, color='#e377c2')
+        ax2.plot(time_range,gauss_wave, color='#e377c2')
+    fig.tight_layout()
+    
+
+
     # Refresh the canvas
     canvas.draw()
+    state_probs = [result.expect[i][-1] for i in range(4)]  
+    State_0.config(text=f"P0: {state_probs[0]*100:.2f}%")
+    State_1.config(text=f"P1: {state_probs[1]*100:.2f}%")
+    State_2.config(text=f"P2: {state_probs[2]*100:.2f}%")
+    State_3.config(text=f"P3: {state_probs[3]*100:.2f}%")
+
+    
 # def const(x):
 #     return x[0]
 # def minimize_func(x):
@@ -195,15 +236,14 @@ def update_plot():
 #     args['sigma'] = x[1]
 #     obj = gate_class.GateEvo(time_range, qpsi0, args).make_result().expect[1][-1]
 #     res = minimize(np.abs(0.5-obj), x0=[0.07,130], constraints=constraints)
-#     return(res.x[0],res.x[1])
-# def chunk_evo():       
+#     return(res.x[0],res.x[1])       
         
 
 # bt_minimize = tk.Button(solution_frame, text="最小化", command=minimize_func(), font=("Arial", 14))
 # bt_minimize.grid(row=0, column=1, padx=5, pady=10, sticky="ew")
 
-bt_plot = tk.Button(GUItest, text="Plot",command=update_plot, font=("Arial", 14)) 
-bt_plot.grid(row=10, column=1, padx=5, pady=10, sticky="ew")
+bt_plot = tk.Button(GUItest, text="Plot",command=update_plot, font=("Arial", 24, "bold")) 
+bt_plot.grid(row=10, column=1, padx=5, pady=20, sticky="ew")
 
 
 GUItest.mainloop()
